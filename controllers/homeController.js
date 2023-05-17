ef4d0d680a7e432ee213acc7601fac43fb468cfc
Jatinchourasia <jatinchourasia3@gmail.com>
2023-05-18 01:10:10 +0530

    redeploy

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,6 +3,6 @@ const PromiseHandler = require("../utils/promiseHandler");
 exports.home = PromiseHandler(async (req, res) => {
   res.status(200).json({
     success: true,
-    greeting: "API is working fine",
+    greeting: "API is working fine.",
   });
 });
